092338ab5f411535616aa5da6ebbdd758dd02f76
akunlainfiqi <fiqi296@gmail.com>
2021-10-23 21:52:57 +0700

    extend tailwind theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,19 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        sans: ['"Inter var"', ...defaultTheme.fontFamily.sans],
+        sans: ['"Poppins"', ...defaultTheme.fontFamily.sans],
+        Montserrat: ['"Montserrat"', ...defaultTheme.fontFamily.sans],
+      },
+      colors:{
+        'main': '#13004A',
+        'gradient-1a': '#F7620E',
+        'gradient-1b': '#FBD104',
+        'gradient-2a': '#42A642',
+        'gradient-2b': '#9ADF6D',
+        'text-main': '#3C3568',
+        'text-secondary': '#797395',
+        'text-tertiary': '#C6C6DE',
+        'accent': '#D8DCFE'
       },
     },
   },
